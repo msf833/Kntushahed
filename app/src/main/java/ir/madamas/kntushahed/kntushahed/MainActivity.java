9c2b180256c23e30a93acb97d9d16359ccc5fd3c
msf833 <msf.case@gmail.com>
2017-05-12 16:44:25 +0430

    Volley library added and implemented

--- app/src/main/java/ir/madamas/kntushahed/kntushahed/MainActivity.java
+++ app/src/main/java/ir/madamas/kntushahed/kntushahed/MainActivity.java
@@ -1,16 +1,37 @@
 package ir.madamas.kntushahed.kntushahed;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
 
+    RequestQueue myrequestqueue;
+    String tempMF;
+
+JSONObject jobg = new  JSONObject();
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -36,10 +57,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myrequestqueue = Volley.newRequestQueue(this);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Button btn_getData = (Button) findViewById(R.id.btn_getData);
+        btn_getData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("MSF","btn request clicked");
+             JsonObjectRequest JsonRequest = new JsonObjectRequest(Request.Method.POST, "http://api.mim-app.ir/SelectValue_profList.php",jobg,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+
+                                try {
+                                    JSONArray array = response.getJSONArray("prof_list");
+                                    Toast.makeText(MainActivity.this, "on responsed", Toast.LENGTH_SHORT).show();
+                                    for (int i =0 ; i < array.length();i++){
+                                        JSONObject temp = array.getJSONObject(i);
+
+                                         tempMF  = temp.getString("ProfessorsID");
+                                    }
+
+                                    Log.i("resp",tempMF);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        }
+
+                );
+                myrequestqueue.add(JsonRequest);
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/ir/madamas/kntushahed/kntushahed/Statics/attributes.java
@@ -0,0 +1,9 @@
+package ir.madamas.kntushahed.kntushahed.Statics;
+
+/**
+ * Created by MSF on 5/12/2017.
+ */
+
+public class attributes {
+   public static int version = 1;
+}
