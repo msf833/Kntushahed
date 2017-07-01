51ba2c07fe5b5dc411436a6426fdad89f6b4bd14
NFP_7037 <ali.ghasemzad@gmail.com>
2017-07-01 08:30:31 +0430

    webView instead of TextView in ExtraCourseActivity

--- app/src/main/java/ir/madamas/kntushahed/kntushahed/MainActivity.java
+++ app/src/main/java/ir/madamas/kntushahed/kntushahed/MainActivity.java
@@ -82,7 +82,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
